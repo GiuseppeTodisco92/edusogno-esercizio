f6956395c49107a55ac5ca191dcd1babbbe72dcc
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-10-19 09:54:38 +0200

    add-pw-box

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <link rel="stylesheet" href="assets/styles/style.css">
     <title>Edusogno</title>
 </head>
@@ -35,8 +37,13 @@
             </div>
 
             <div class="input-box">
-                <label for="password">Inserisci la password</label>
-                <input type="password" name="password" id="password" placeholder="Scrivila qui" required>
+                <div>
+                    <label for="password">Inserisci la password</label>
+                </div>
+                <div class="password">
+                    <input type="password" name="password" id="password" placeholder="Scrivila qui" required>
+                    <i class="fa-solid fa-eye " id="eye"></i>
+                </div>
             </div>
 
             <input class="btn" type="submit" value = "registrati">
